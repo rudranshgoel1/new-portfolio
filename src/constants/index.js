efc594f8739209e082e3d9e688af1eff85bc0604
rudranshgoel1 <app.whatsupleads@gmail.com>
2026-03-15 17:06:59 +0530

    Remove old resume PDF, add new gallery images, and enhance Photos component with sidebar and icons

--- src/constants/index.js
+++ src/constants/index.js
@@ -178,19 +178,19 @@ const photosLinks = [
 const gallery = [
   {
     id: 1,
-    img: "/images/gal1.png",
+    img: "/images/rudransh-3.JPG",
   },
   {
     id: 2,
-    img: "/images/gal2.png",
+    img: "/images/rudransh-2.jpg",
   },
   {
     id: 3,
-    img: "/images/gal3.png",
+    img: "/images/rudranshlogo.png",
   },
   {
     id: 4,
-    img: "/images/gal4.png",
+    img: "/images/rudranshabt.jpg",
   },
 ];
 
